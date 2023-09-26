e68b57121e3cd36452056f4c8acb5c076c40641c
rafgani <afganirizal@gmail.com>
2023-09-26 19:48:57 +0000

    Add files via upload

---
+++ xmonad.hs
@@ -0,0 +1,97 @@
+import XMonad
+  
+import XMonad.Config.Xfce
+
+--  import Colors.DoomOne
+
+
+import XMonad.Layout.Spiral
+import Data.Ratio -- this makes the '%' operator available (optional)
+import XMonad.Layout.Spacing
+import XMonad.Layout.Grid
+import XMonad.Layout.Gaps
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+import XMonad.Hooks.EwmhDesktops
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Loggers
+import XMonad.Util.Ungrab
+
+
+
+myLayouts =  spacing 5 $ magnifier $ Tall 1 (3/100) (1/2) ||| spiral (125 % 146) ||| Grid ||| Mirror (Tall 1 (3/100) (3/5)) ||| Full
+
+-- gaps [(U,30)] $
+
+-- main = xmonad . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey xfceConfig
+--         { terminal = "kitty"
+--         , modMask = mod4Mask -- optional: use Win key instead of Alt as MODi key
+--	 , layoutHook = myLayouts
+--         }
+
+main :: IO ()
+main = xmonad
+       -- . ewmhFullscreen
+       -- . ewmh
+       . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
+
+myConfig = xfceConfig
+    { terminal = "kitty"
+    , modMask    = mod4Mask      -- Rebind Mod to the Super key
+    , layoutHook = myLayouts
+    -- Use custom layouts
+    , manageHook = myManageHook  -- Match on certain windows
+    }
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , isDialog            --> doFloat
+    ]
+
+myLayout =   spacing 5 $ tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = magnifier $ ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 10
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
+   
