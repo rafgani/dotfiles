178d5205c640669a3e7f958d5bd9e7b5a5a237c2
rafgani <afganirizal@gmail.com>
2023-10-02 22:28:58 +0700

    Add files via upload

--- xmonad.hs
+++ xmonad.hs
@@ -1,31 +1,131 @@
+------------------------------------------------------------------------
+-- IMPORTS
+------------------------------------------------------------------------
+    -- Base
 import XMonad
-  
+import System.IO (hPutStrLn)
+import System.Exit (exitSuccess)
+import qualified XMonad.StackSet as W
 import XMonad.Config.Xfce
 
 --  import Colors.DoomOne
 
 
-import XMonad.Layout.Spiral
-import Data.Ratio -- this makes the '%' operator available (optional)
-import XMonad.Layout.Spacing
-import XMonad.Layout.Grid
-import XMonad.Layout.Gaps
-import XMonad.Layout.ThreeColumns
-import XMonad.Layout.Magnifier
 
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
+    -- Actions
+import XMonad.Actions.CopyWindow (kill1, killAllOtherCopies)
+import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
+import XMonad.Actions.GridSelect
+import XMonad.Actions.MouseResize
+import XMonad.Actions.Promote
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
+import qualified XMonad.Actions.TreeSelect as TS
+import XMonad.Actions.WindowGo (runOrRaise)
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import qualified XMonad.Actions.Search as S
+
+    -- Data
+import Data.Char (isSpace)
+import Data.Monoid
+import Data.Maybe (isJust)
+import Data.Tree
+-- import qualified Data.Tuple.Extra as TE
+import qualified Data.Map as M
+import Data.Ratio -- this makes the '%' operator available (optional)
+
+    -- Hooks
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ServerMode
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.WorkspaceHistory
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
-import XMonad.Hooks.EwmhDesktops
+    -- Layouts
+-- import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spiral
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Grid
+import XMonad.Layout.Gaps
 
-import XMonad.Util.EZConfig
+    -- Layouts modifiers
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.Magnifier
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Spacing
+import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
+
+    -- Prompt
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Prompt.FuzzyMatch
+import XMonad.Prompt.Man
+import XMonad.Prompt.Pass
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Prompt.Ssh
+import XMonad.Prompt.XMonad
+import Control.Arrow (first)
+
+    -- Utilities
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.SpawnOnce
+
 import XMonad.Util.Loggers
 import XMonad.Util.Ungrab
 
+------------------------------------------------------------------------
+-- VARIABLES
+------------------------------------------------------------------------
+-- It's nice to assign values to stuff that you will use more than once
+-- in the config. Setting values for things like font, terminal and editor
+-- means you only have to change the value here to make changes globally.
+myFont :: String
+myFont = "xft:Fantasque Sans Mono:regular:size=12"
 
+myModMask :: KeyMask
+myModMask = mod4Mask       -- Sets modkey to super/windows key
+
+myTerminal :: String
+myTerminal = "kitty"   -- Sets default terminal
+
+myBrowser :: String
+-- myBrowser = myTerminal ++ " -e lynx "  -- Sets lynx as browser for tree select
+myBrowser = "firefox "                 -- Sets firefox as browser for tree select
+
+myEditor :: String
+myEditor = "emacsclient -c -a emacs "  -- Sets emacs as editor for tree select
+-- myEditor = myTerminal ++ " -e vim "    -- Sets vim as editor for tree select
+
+myBorderWidth :: Dimension
+myBorderWidth = 2          -- Sets border width for windows
+
+myNormColor :: String
+myNormColor   = "#292d3e"  -- Border color of normal windows
+
+myFocusColor :: String
+myFocusColor  = "#bbc5ff"  -- Border color of focused windows
+
+altMask :: KeyMask
+altMask = mod1Mask         -- Setting this for use in xprompts
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 myLayouts =  spacing 5 $ magnifier $ Tall 1 (3/100) (1/2) ||| spiral (125 % 146) ||| Grid ||| Mirror (Tall 1 (3/100) (3/5)) ||| Full
 
@@ -41,12 +141,12 @@ main :: IO ()
 main = xmonad
        -- . ewmhFullscreen
        -- . ewmh
-       . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+       .withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
      $ myConfig
 
 myConfig = xfceConfig
-    { terminal = "kitty"
-    , modMask    = mod4Mask      -- Rebind Mod to the Super key
+    { terminal   = myTerminal
+    , modMask    = myModMask      -- Rebind Mod to the Super key
     , layoutHook = myLayouts
     -- Use custom layouts
     , manageHook = myManageHook  -- Match on certain windows
@@ -54,8 +154,11 @@ myConfig = xfceConfig
 
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ className =? "Gimp" --> doFloat
-    , isDialog            --> doFloat
+    [ className =? "Gimp"                --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+    , isDialog                           --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+    , className =? "Xfce4-appfinder"     --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+    -- , className =? "lyx"                 --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+    -- , className =? "texmacs.bin"         --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
     ]
 
 myLayout =   spacing 5 $ tiled ||| Mirror tiled ||| Full ||| threeCol
@@ -94,4 +197,4 @@ myXmobarPP = def
     red      = xmobarColor "#ff5555" ""
     lowWhite = xmobarColor "#bbbbbb" ""
 
-   
+
